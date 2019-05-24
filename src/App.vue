76057455d3b98e2bd37026324a150982dbd4ccd3
antonellam <antonella.michelli@gmail.com>
2019-05-24 11:37:25 +0200

    Fix bugs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <SelectedItem 
-    v-for="state in selectedStates" v-bind:key="state"
-    v-on:select-state="selectedStates" :del-selected="deleteSelected"/>
-    <Multiselector v-bind:states="states" />
+      <div class="section__selection">
+        <SelectedItem
+          v-bind:selectedStates="selectedStates"
+          v-on:del-selected="deleteSelected($event)"
+        />
+        <Multiselector v-bind:states="states" v-on:select-state="selectState($event)"/>
+      </div>
   </div>
 </template>
 
 <script>
-import Multiselector from './components/Multiselector.vue'
-import SelectedItem from './components/SelectedItem.vue'
+import Multiselector from "./components/Multiselector.vue";
+import SelectedItem from "./components/SelectedItem.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Multiselector,
     SelectedItem
@@ -21,35 +24,64 @@ export default {
   data() {
     return {
       states: [],
-      selectedStates: [],
-    }
+      selectedStates: []
+    };
   },
 
   methods: {
-    selectState(selectedState) {
-      this.selectedStates = this.selectedStates.push(selectedState);
+    selectState(state) {
+      if (this.selectedStates.includes(state)) {
+        this.selectedStates = this.selectedStates.filter(
+        selectedState => selectedState !== state
+      )} else {
+        this.selectedStates = [...this.selectedStates, state];
+      }
     },
 
-    deleteSelected(deletedState) {
-      this.selectedStates = this.selectedStates.filter(state => state === selectedState);
+    deleteSelected(state) {
+      this.selectedStates = this.selectedStates.filter(
+        selectedState => selectedState !== state
+      );
     }
   },
 
   created() {
-    fetch('https://raw.githubusercontent.com/AntonellaM/american-states-json/master/db.json')
+    fetch(
+      "https://raw.githubusercontent.com/AntonellaM/american-states-json/master/db.json"
+    )
       .then(res => res.json())
-      .then(states => this.states = states)
+      .then(states => (this.states = states));
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  body {
+    *, *::before, *::after {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+  }
+  .section {
+    &__selection {
+      width: 220px;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+      justify-content: center;
+      align-items: center;
+    }
   }
 </style>
--- src/components/Multiselector.vue
+++ src/components/Multiselector.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="hello">
-    <h1>Countries</h1>
-    <select v-model="selectedStates" multiple>
+    <select 
+    v-model="states"
+    placeholder="Select a state:"
+    multiple>
+      <option disabled selected>Choose your state</option>
       <option
-        v-for="state in states" :value="state" :name="state"
+        v-for="state in states"
+        :value="state"
         v-bind:key="state"
         @click="$emit('select-state', state)"
       >{{ state }}</option>
@@ -14,23 +18,40 @@
 <script>
 export default {
   name: "Multiselector",
-  props: ["states", "selectedStates"],
+  props: ["states"]
 };
 </script>
 
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+    /* width */
+  select ::-webkit-scrollbar {
+    width: 1px;
+  }
+
+  /* Track */
+  select ::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+  }
+
+  /* Handle */
+  select ::-webkit-scrollbar-thumb {
+    background: #888; 
+  }
+
+  /* Handle on hover */
+  select ::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+  }
+
+  select {
+    border-radius: 5px;
+    
+  }
+  option {
+    height: 30px;
+    width: 200px;
+    padding: 8px;
+    display: flex;
+    align-items: center; 
+  }
 </style>
