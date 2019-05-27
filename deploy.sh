6eece2c47af41c862002ec8a63cee04962a74ded
antonellam <antonella.michelli@gmail.com>
2019-05-27 17:50:51 +0200

    Delete docs folder

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ git add -A
 git commit -m 'deploy'
 
 # # if you are deploying to https://<USERNAME>.github.io
-# git push -f https://github.com/antonellam/tadaima-coding-test-vue.git master:gh-pages
+git push -f https://github.com/antonellam/tadaima-coding-test-vue.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://antonellam.github.io/tadaima-coding-test-vue.git master:gh-pages
+# git push -f https://antonellam.github.io/tadaima-coding-test-vue.git master:gh-pages
 
 # git push -f git@github.com:<antonellam>/<tadaima-coding-test-vue>.git master:gh-pages
 
