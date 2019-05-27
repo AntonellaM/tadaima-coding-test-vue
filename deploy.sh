6b25e58091fe48c5bca21e08cc5e95db7c2c3115
antonellam <antonella.michelli@gmail.com>
2019-05-27 17:37:08 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,12 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<antonellam>/<antonellam>.github.io.git master
+# # if you are deploying to https://<USERNAME>.github.io
+# git push -f https://github.com/antonellam/tadaima-coding-test-vue.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<antonellam>/<tadaima-coding-test-vue>.git master:gh-pages
+git push -f https://antonellam.github.io/tadaima-coding-test-vue.git master:gh-pages
+
+# git push -f git@github.com:<antonellam>/<tadaima-coding-test-vue>.git master:gh-pages
 
 cd -
