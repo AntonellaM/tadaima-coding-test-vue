a87193583e968f387eaa1ed8bbb082d401feb565
antonellam <antonella.michelli@gmail.com>
2019-05-27 22:13:53 +0200

    Build docs folder

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <div class="multiselect__component-wrapper">
-      <label class="visually-hidden typo__label">Selecciona un estado</label>
-      <multiselect 
-        v-model="value"
-        placeholder="Selecciona o introduce un estado"
-        label="name"
-        track-by="code"
-        :options="options"
-        :multiple="true"
-        :taggable="true"
-        @tag="addState"
-      >
-      </multiselect>
-    </div>
+    <header class="header">
+      <div class="header__logo"></div>
+      <h1 class="header__title">
+        Tadaima Coding Test
+      </h1>
+    </header>
+    <main>
+      <div class="multiselect__component-wrapper">
+        <label class="visually-hidden typo__label">Selecciona un estado</label>
+        <multiselect 
+          v-model="value"
+          placeholder="Selecciona o introduce un estado"
+          label="name"
+          track-by="code"
+          :options="options"
+          :multiple="true"
+          :taggable="true"
+          @tag="addState"
+        >
+        </multiselect>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -59,7 +67,35 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+
+  body {
+    min-height: 100vh;
+    background-image: linear-gradient(-45deg, #FF7555, #FF5572);
+  }
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    &__logo {
+      height: 50px;
+      width: 50px;
+      margin: 0 20px;
+      background-image: url("../../assets/logo.jpg");
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+    }
+
+    &__title {
+      color: #fff;
+      font-size: 35px;
+      font-family: 'Petit Formal Script', cursive;
+    }
+  }
 
   .multiselect__component-wrapper {
     max-width: 300px;
@@ -68,6 +104,10 @@ export default {
 
   .multiselect {
     margin-top: 20px;
+  }
+
+  .multiselect__tag, .multiselect__element span {
+    color: black;
   }
 
   .visually-hidden {
